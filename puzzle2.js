34f9192bd2919775917bfc6b67d94fde7410939b
Dzampathas <35543030+Dzampathas@users.noreply.github.com>
2018-01-21 11:05:56 -0800

    Create puzzle2.js

---
+++ puzzle2.js
@@ -0,0 +1,31 @@
+var count = 0;
+
+while(count < 6) {
+  down();
+  count += count + 1;
+}
+count = 0;
+
+while(count < 3) {
+  right();
+  count += count + 1;
+}
+count = 0;
+
+while(count < 2) {
+  up();
+  count += count + 1;
+}
+count = 0;
+
+while(count < 2) {
+  right();
+  count += count +1;
+}
+count = 0;
+
+down();
+
+while(count < 2) {
+  down();
+}
