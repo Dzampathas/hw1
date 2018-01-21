58e9c1bfbd559e6bc6301c35514c9bef46ec8eeb
Dzampathas <35543030+Dzampathas@users.noreply.github.com>
2018-01-21 11:28:44 -0800

    Create puzzle8.js

---
+++ puzzle8.js
@@ -0,0 +1,30 @@
+var count = 0;
+
+function moveDown() {
+  while(count < 8) {
+    down();
+  }
+}
+
+function moveUp() {
+  while(count < 8) {
+    up();
+  }
+}
+
+moveDown();
+right();
+right();
+moveUp();
+right();
+right();
+moveDown();
+right();
+right();
+moveUp();
+right();
+right();
+moveDown();
+right;
+
+
