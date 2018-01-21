c4a76d41da6fdd94d036d08b4a7d06519561be0e
Dzampathas <35543030+Dzampathas@users.noreply.github.com>
2018-01-21 11:01:01 -0800

    Update puzzle1.js

--- puzzle1.js
+++ puzzle1.js
@@ -1,1 +1,26 @@
 
+var count = 0;
+//move down
+while(count<7) {
+ down();
+  count += count + 1;
+}
+count = 0;
+//move right
+while(count<5) {
+  right();
+  count += count + 1;
+}
+count = 0;
+//move up
+while(count<2) {
+ up();
+  count += count + 1;
+}
+count = 0;
+//move right
+while(count<2) {
+ right();
+  count += count + 1;
+}
+count = 0;
