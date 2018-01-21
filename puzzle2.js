97af271d6fee5b69d3a412e66935b2e242465685
Dzampathas <35543030+Dzampathas@users.noreply.github.com>
2018-01-21 11:09:45 -0800

    Update puzzle2.js

--- puzzle2.js
+++ puzzle2.js
@@ -2,25 +2,25 @@ var count = 0;
 
 while(count < 6) {
   down();
-  count += count + 1;
+  count = count + 1;
 }
 count = 0;
 
 while(count < 3) {
   right();
-  count += count + 1;
+  count = count + 1;
 }
 count = 0;
 
 while(count < 2) {
   up();
-  count += count + 1;
+  count = count + 1;
 }
 count = 0;
 
 while(count < 2) {
   right();
-  count += count +1;
+  count = count +1;
 }
 count = 0;
 
@@ -28,4 +28,5 @@ down();
 
 while(count < 2) {
   down();
+  count = count +1;
 }
